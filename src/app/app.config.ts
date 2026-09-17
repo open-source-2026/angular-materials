f072c4327e9ffe519c78bce3aa0558989cf65191
Yamil Tejada <yamil.tejada9.pumacayo10@gmail.com>
2026-09-17 06:29:49 -0500

    feat(i18n): add ngx-translate for internationalization support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,18 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
+import {provideTranslateLoader, provideTranslateService} from '@ngx-translate/core';
+import {provideTranslateHttpLoader} from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(), // This way you can use HttpClient in your application
+
+    // Define the route for the app working with i18n
+    provideTranslateService({
+        loader: provideTranslateHttpLoader({prefix: './i18n/', suffix: '.json'}),
+        fallbackLang: 'en'
+    })
   ]
 };
